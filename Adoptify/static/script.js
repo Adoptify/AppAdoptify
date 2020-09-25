c94643caff7c99a28c06988b09b20b63cb54544f
Juan Ignacio Dominguez <juanignaciodom3@gmail.com>
2020-09-25 20:54:54 -0300

    detalles form

--- Adoptify/static/script.js
+++
@@ -1,7 +0,0 @@
-const descripcion = document.getElementsByName('textarea');
-console.log(descripcion);
-descripcion.addEventListener('input', (evt) => {
-	console.log('holaa');
-	const display = document.getElementById('display');
-	display.textContent = `${evt.target.value.length}/300`;
-});
