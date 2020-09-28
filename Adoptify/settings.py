88044c418500a4126996c392356a11b298e04b2e
Luna Armando Gabriel <armandogabrielluna@gmail.com>
2020-09-28 17:17:19 -0300

    intentando

--- Adoptify/settings.py
+++ Adoptify/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_ix+1m04#1p#$(+j&ttbrx#=2!wlydtb!ecq98tq#x-5z$bjms'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'appMascotas',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'Adoptify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demo_1',
+        'USER': 'armando',
+        'PASSWORD': 'armando123',
+        'HOST': 'database-1.co3j44kwdykz.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'es-es'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
@@ -128,3 +133,39 @@ STATICFILES_DIRS = (
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+
+AWS_ACCESS_KEY_ID = 'AKIASM5SLXBOQASGK2RN'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'armando-adoptify-bucket'
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_REGION_NAME = 'us-east-2'  # change to your region
+
+# USE_S3 = os.getenv('USE_S3') == 'TRUE'
+
+# if USE_S3:
+#     # aws settings
+#     AWS_ACCESS_KEY_ID = os.getenv('AKIASM5SLXBOZHSTZQXM')
+#     AWS_SECRET_ACCESS_KEY = os.getenv('LqShvgO6btaf02d7WRuOZsr3/6/iAZ+niVy4cRYF')
+#     AWS_STORAGE_BUCKET_NAME = os.getenv('armando-adoptify-bucket')
+#     AWS_DEFAULT_ACL = None
+#     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+#     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+#     # s3 static settings
+#     STATIC_LOCATION = 'static'
+#     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+#     STATICFILES_STORAGE = 'hello_django.storage_backends.StaticStorage'
+#     # s3 public media settings
+#     PUBLIC_MEDIA_LOCATION = 'media'
+#     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_STATIC_LOCATION}/images'
+#     DEFAULT_FILE_STORAGE = 'hello_django.storage_backends.PublicMediaStorage'
+# else:
+#     STATIC_URL = '/static/'
+#     MEDIA_URL = '/images/'
+#     MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
